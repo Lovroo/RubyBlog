58d652c79ed0e907580d25cc03c01fbae70eff43
lovro <lovro.napotnik@scv.si>
2021-11-09 20:04:02 +0100

    Fix helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,15 +3,16 @@ module PostsHelper
     capture do
     concat link_to('Preberi Več', post, class: 'stretched-link')
     if current_user == post.user
-      concat  link_to('Uredi', edit_post_path(post), class: 'stretched-link')
-      concat  link_to('Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' }, class: 'stretched-link')
+
       end
     end
   end
   def display_show_post_links(post)
     capture do
     if current_user == post.user
-      concat  link_to 'Edit', edit_post_path(post)
+      concat  link_to('Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' })
+      concat ' | '
+      concat  link_to 'Uredi', edit_post_path(post)
       concat ' | '
     end
     concat link_to 'Back', posts_path
