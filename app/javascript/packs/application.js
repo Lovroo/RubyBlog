5ddbd4d4c93780107ff61abb700abbe5665fd73b
lovro <lovro.napotnik@scv.si>
2021-11-11 13:24:20 +0100

    Add date time picker!

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,19 @@ import 'scss/site'// JS
 import('js/site')// Images
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
+// Importing stimulus
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+
+// Import style for flatpickr
+require("flatpickr/dist/flatpickr.css")
+
+// Manually register Flatpickr as a stimulus controller
+application.register('flatpickr', Flatpickr)
